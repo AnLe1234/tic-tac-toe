7fb285c3e490506cfe90184d2701480d6782cf8f
An Le <anle1@cpp.edu>
2022-10-05 19:25:13 -0700

    First iteration

---
+++ tictactoe.c
@@ -0,0 +1,21 @@
+// tic tac toe between a computer or 2 people
+
+// fucntion to display current board
+
+// main function
+int main() {
+    // user choose which type to play
+
+    // read input to determine which type
+
+    // loop back until 1 person win/board is full
+        // player 1 choose a location
+        // display board
+        // check if win condition/full board
+        // player 2 choose a location
+        // displayboard
+        // check if win condition/full board
+        // print out winner/draw
+
+    return 0;
+}
